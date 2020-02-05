7dc06ebb1a7f6d95e77e2c8e76bc36150c856a1e
kikikanik <55113869+kikikanik@users.noreply.github.com>
2020-02-04 20:26:00 -0500

    Did not try, hope it works. Laptop is almost dead.

--- src/Code.java
+++ src/Code.java
@@ -1,9 +1,51 @@
+import java.util.Scanner;
 
 public class Code {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Create a BMI (Body Mass Index) calculator which prompts the user to enter their height (in feet and inches) and their weight (in pounds). The program should give the BMI results (number and category) in a user friendly way.
+		//BMI = kg/m2
+		//m = in/39.37
+		//kg = lb/2.205
 
+		//BMI	Weight Status
+		//Below 19.0	Underweight
+		//19 – 24.9	Normal or Healthy Weight
+		//25.0 – 29.9	Overweight
+		//30.0 – 39.9	Obese
+		//40.0 and Above	Extremely Obese 
+			double pounds = 0;
+			double height = 0;
+			
+			Scanner in = new Scanner(System.in);
+			
+			int tryAgain = 1 ;
+			while(tryAgain != 0) {
+			System.out.println("BMI Calculator. Please enter your weight in pounds: ");
+			pounds = in.nextDouble();
+			conversionForWeight = pounds/2.205; 
+			System.out.println("Please enter your height in inches: ");
+			height = in.nextDouble(); 
+			conversionForHeight = height/39.37; 
+			
+			if (Math.pow(pounds/39.37,  2) / height <= 18.99) { 
+				System.out.println("Your BMI weight status is Underweight.");
+			}
+			else if (Math.pow(pounds, 2) / height <= 24.9) {
+				System.out.println("Your BMI weight status is Normal. You are at a healthy weight.");
+			}
+			else if (Math.pow(pounds,  2) / height <= 25.0) {
+				System.out.println("Your BMI weight status is Overweight.");
+			}
+			else if (Math.pow(pounds,  2) / height <= 30.0) {
+			System.out.println("Your BMI weight status is Obese.");
+			}
+			else if (Math.pow(pounds,  2) / height >= 40.0) { 
+			System.out.println("Your BMI weight status is Extremely Obese.");
+			}
+			System.out.println("To exit the BMI Calulator, Enter 0. To continue, enter any whole number: "); 
+			tryAgain = in.nextInt(); 
+			}
+			in.close();
+		}
 	}
-
-}
